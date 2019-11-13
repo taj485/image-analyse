b1db8ff71b94fb998dac40ab115ba91089dea329
tislam123 <tajbiul.islam@bglgroup.co.uk>
2019-11-13 00:03:41 +0000

    S001 - updated project to latest

--- image-ai-analyser/Controllers/HomeController.cs
+++ image-ai-analyser/Controllers/HomeController.cs
@@ -9,11 +9,20 @@ using System.Web.Mvc;
 using image_ai_analyser.Models;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using image_ai_analyser.Interfaces;
 
 namespace image_ai_analyser.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IImageViewModelBuilder _imageViewModelBuilder;
+
+
+        public HomeController(IImageViewModelBuilder imageViewModelBuilder)
+        {
+            _imageViewModelBuilder = imageViewModelBuilder;
+        }
+
         // GET: Home
         public ActionResult Index()
         {
@@ -30,10 +39,10 @@ namespace image_ai_analyser.Controllers
                 var fileName = Path.GetFileName(file.FileName);
                 var filePath = Path.Combine(Server.MapPath("~/images"), fileName);
                 file.SaveAs(filePath);
-                string jsonString = await MakeAnalysisRequest(filePath);
+
+                string jsonString = await  _imageViewModelBuilder.MakeAnalysisRequest(filePath);
 
                 List<ImageViewModel> model = new List<ImageViewModel>();
-
                 model.Add(Newtonsoft.Json.JsonConvert.DeserializeObject<ImageViewModel>(jsonString));
                 model.First().Image = file; 
 
@@ -52,54 +61,6 @@ namespace image_ai_analyser.Controllers
         public ActionResult ImageResult()
         {
             return View();
-        }
-
-        private async Task<string> MakeAnalysisRequest(string filePath)
-        {
-
-            string subscriptionKey = "4c0a252e509347d8b33ff4b18ee5fc13";
-
-            string endpoint = "https://uksouth.api.cognitive.microsoft.com/";
-
-            string uriBase = endpoint + "vision/v2.1/analyze";
-
-            var client = new HttpClient();
-
-            client.DefaultRequestHeaders.Add(
-                "Ocp-Apim-Subscription-Key", subscriptionKey);
-
-            string requestParameters =
-                "visualFeatures=Categories,Description,Color";
-
-            string uri = uriBase + "?" + requestParameters;
-
-            HttpResponseMessage response;
-
-            byte[] byteData = ConvertToBytesArray(filePath);
-
-            using (ByteArrayContent content = new ByteArrayContent(byteData))
-            {
-                content.Headers.ContentType =
-                    new MediaTypeHeaderValue("application/octet-stream");
-
-                // Asynchronously call the REST API method.
-                response = await client.PostAsync(uri, content);
-            }
-
-            string jsonString = await response.Content.ReadAsStringAsync();
-            return jsonString;
-
-        }
-
-
-        private byte[] ConvertToBytesArray(string file)
-        {
-            using (FileStream fileStream =
-                new FileStream(file, FileMode.Open, FileAccess.Read))
-            {
-                BinaryReader binaryReader = new BinaryReader(fileStream);
-                return binaryReader.ReadBytes((int)fileStream.Length);
-            }
         }
     }
 }
